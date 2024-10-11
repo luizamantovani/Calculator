115125cc85bf00cd21bc2f503560b48536a0c103
luizamantovani <luizamantovani2006@gmail.com>
2024-10-11 19:04:01 -0300

    allowed the usage of keyboard keys

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 
-function apear(input){
+function appear(input){
     display.value += input;
     adjustScroll();
 }
@@ -24,3 +24,19 @@ function adjustScroll() {
 function clearLastDigit() {
     display.value = display.value.slice(0, -1); 
 }
+
+document.addEventListener('keydown', function(event){
+    const isNumber = /^[0-9]$/i.test(event.key);
+    const operator = ['+', '-', '/', '*'];
+    if(isNumber || operator.includes(event.key)){
+        appear(event.key);
+    }
+
+    if(event.key === 'Enter'){
+        calculate();
+    }
+
+    if(event.key === 'Backspace'){
+        clearLastDigit();
+    }
+});
